bf37c5b2906f59b220d537f822fd2a14ff574a66
heyuhao <ranhaogg@qq.com>
2020-11-10 14:00:45 +0800

    单选多选修改

--- resources/views/question/index.blade.php
+++ resources/views/question/index.blade.php
@@ -4,7 +4,9 @@
 
 
 <table class="table table-condensed">
+
   <caption>题库展示</caption>
+  <button type="submit"  style="float:right" class="btn btn-default"><a href="{{url('/question/huifuindex')}}">恢复已删除</a></button>  
   <thead>
     <tr>
       <th>id</th>
@@ -26,12 +28,31 @@
       <td>@if($v->question_type_id==1)单选题  @elseif($v->question_type_id==2)多选题  @elseif($v->question_type_id==3)简答题 @endif</td>
       <td>@if($v->question_diff==1)简单  @elseif($v->question_diff==2)中等  @elseif($v->question_diff==3)困难 @endif</td>
       <td>{{$v->question_name}}</td>
-      <td>@if($v->question_cor==1)A @endif @if($v->question_cor==2)B @endif @if($v->question_cor==3)C @endif @if($v->question_cor==4)D @endif</td>
+      @if($v->question_type_id==1) 
+        <td>@if($v->question_cor==1)A @endif @if($v->question_cor==2)B @endif @if($v->question_cor==3)C @endif @if($v->question_cor==4)D @endif</td>
+      @elseif($v->question_type_id==2)
+        <td>
+            @if($v->question_cor==1 || $v->question_cor==2)
+              A,B
+            @elseif($v->question_cor==1 || $v->question_cor==3)
+              A,C
+            @elseif($v->question_cor==1  || $v->question_cor==4)
+              A,D
+            @elseif($v->question_cor==2  || $v->question_cor==3)
+              B,C
+            @elseif($v->question_cor==2  || $v->question_cor==4)
+              B,D
+            @elseif($v->question_cor==3  || $v->question_cor==4)
+              C,D
+            @endif
+        </td>
+      @endif
       <td>{{$v->cor_a}}</td>
       <td>{{$v->cor_b}}</td>
       <td>{{$v->cor_c}}</td>
       <td>{{$v->cor_d}}</td>
       <td><!-- <a href="{{url('/question/del/'.$v->question_id)}}"></a> -->
+        <button type="submit" id="del" value="{{$v->question_id}}" class="btn btn-default"><a href="{{url('/question/course/'.$v->question_id)}}">关联课程</a></button>  
       	<button type="submit" id="del" value="{{$v->question_id}}" class="btn btn-default">删除</button>	
         <button type="submit" id="upd" value="{{$v->question_id}}" class="btn btn-default"><a href="{{url('/question/upd/'.$v->question_id)}}">修改</a></button>
       </td>
