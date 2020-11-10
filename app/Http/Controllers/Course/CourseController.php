9546343ea1b7245bb81f8f189d75c7970a0c78fd
zhaoyi <1113252039@qq.com>
2020-11-10 21:24:06 +0800

    光棍节前夕加班

--- app/Http/Controllers/Course/CourseController.php
+++ app/Http/Controllers/Course/CourseController.php
@@ -20,7 +20,6 @@ class CourseController extends Controller
         //查询课程分类信息
         $type_data=CourseTypeModel::select('type_id','type_name')->get();
         //查询讲师信息
-
         return view('course.create',compact('type_data'));
     }
     /**
@@ -56,6 +55,15 @@ class CourseController extends Controller
             ['course_del','=',0]
         ];
         $course_data=CourseModel::leftjoin('course_type','course.course_type','=','course_type.type_id')->leftjoin('lect','course.lect_id','=','lect.lect_id')->where($course_where)->get();
+        // $array=[];
+        // foreach($course_data as $v){
+        //     $v->pid=0;
+        //     //查询章程
+        //     $chapter_data=CourseChapterModel::where('course_id',$v->course_id)->get();
+        //     $chapter_data['pid']=$v->course_id;
+
+        // }
+        // dd($array);
         return view('course.list',compact('course_data'));
     }
     //图片上传处理
@@ -95,6 +103,7 @@ class CourseController extends Controller
             ['chapter_id','=',$chapter_id]
         ];
         $section_data=CourseSectionModel::where($section_where)->get()->toArray();
+        // dd($section_data);
         return json_encode($section_data);
     }
       /**
@@ -111,7 +120,24 @@ class CourseController extends Controller
             ['section_id','=',$section_id]
         ];
         $class_data=CourseClassModel::where($class_where)->get()->toArray();
+        // dd($class_data);
         return json_encode($class_data);
+    }
+    public function del(){
+        $course_id=request()->course_id;
+        //判断删除的课程下是否有章程
+        $chapter_data=CourseChapterModel::where('course_id',$course_id)->first();
+        // dd($chapter_data);
+        if(!empty($chapter_data)){
+            return ['code'=>0002,'msg'=>'本课程下有章程，不可以删除'];die;
+        }
+        //逻辑删
+        $res=CourseModel::where('course_id',$course_id)->update(['course_del'=>1]);
+        if($res){
+            return ['code'=>0001,'msg'=>'删除成功'];
+        }else{
+            return ['code'=>0002,'msg'=>'删除失败'];
+        }
     }
 
 
