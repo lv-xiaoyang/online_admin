35f80d1af0aadf50b16065ed524c9ec27a5e3631
heyuhao <ranhaogg@qq.com>
2020-11-13 15:24:25 +0800

    优化查看考题页面

--- resources/views/exam/questionindex.blade.php
+++ resources/views/exam/questionindex.blade.php
@@ -14,7 +14,7 @@
       <th>题型</th>
       <th>小题题目</th>
       <th>添加时间</th>
-      <th>正确答案</th>
+      <!-- <th>正确答案</th> -->
       <th>操作</th>
   </tr>
   </thead>
@@ -26,7 +26,7 @@
       <td>@if($v->question_type_id==1)单选题 @elseif($v->question_type_id)多选题 @elseif($v->question_type_id==3)简答题 @endif</td>
       <td>{{$v->question_name}}</td>
       <td>{{date('Y-m-d H:i:s',$v->eq_time)}}</td>
-      <td>{{$v->question_cor}}</td>
+      <!-- <td>{{$v->question_cor}}</td> -->
       <td>
       	<button class="btn btn-info" value="{{$v->exam_id}}"><a href="{{url('/exam/delete/'.$v->eq_id)}}">删除</a></button>
       </td>
