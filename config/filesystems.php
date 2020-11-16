1ce60c93fa0cb35e85ce6aeb2dcc521df100420f
zhaoyi <1113252039@qq.com>
2020-11-16 11:29:55 +0800

    .

--- config/filesystems.php
+++ config/filesystems.php
@@ -45,15 +45,15 @@ return [
 
         'local' => [
             'driver' => 'local',
-            'root' => storage_path('app'),
+            'root' => public_path(),
         ],
 
         'public' => [
             'driver' => 'local',
             // 'root' => storage_path('app/public'),
-            'root' => '/images',
+            'root' =>   public_path(),
             // 'url' => env('APP_URL').'/storage',
-            'url' => env('APP_URL'),
+            'url' => env('APP_URL').'/images',
             'visibility' => 'public',
         ],
 
